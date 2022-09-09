4f66c7d7bd1148d5adbf258ab22af3714b25bec4
HuangLinBao <tariq.92@live.com>
2022-09-09 11:42:55 +0300

    noice

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,10 @@
 const router = require("express").Router();
 const User = require("../models/User");
-//validation
-const Joi = require("joi");
-
-const schema = Joi.object({
-    name: Joi.string().min(6).required(),
-    email: Joi.string().min(6).required().email(),
-    password: Joi.string().min(6).required(),
-});
+const { registerValidation, loginValidation } = require("../Validation");
 
 router.post("/register", async(req, res) => {
-    //validation
-    const { error } = schema.validate(req.body);
+    const { error } = registerValidation(req.body);
     if (error) return res.status(400).send(error.details[0].message);
-
     const user = new User({
         name: req.body.name,
         email: req.body.email,
--- validation.js
+++
@@ -1,7 +0,0 @@
-const Joi = require("joi");
-
-const schema = Joi.object({
-    name: Joi.string().min(6).required(),
-    email: Joi.string().min(6).required().email(),
-    password: Joi.string().min(6).required(),
-});
