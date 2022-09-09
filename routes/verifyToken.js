23d61d247d55231643ecfbc494716a29ab8f448f
HuangLinBao <tariq.92@live.com>
2022-09-09 19:08:55 +0300

    aaaaaannnd done with jwt

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-function auth(req, res, next) {
+module.exports = function auth(req, res, next) {
     const token = req.header("auth-token");
     if (!token) {
         return res.status(401).send("Unauthorized");
@@ -13,4 +13,4 @@ function auth(req, res, next) {
     } catch (e) {
         return res.status(400).send("Invalid Credentials");
     }
-}
+};
