c135b227326837966ff158566eb1ecf497e7c477
HuangLinBao <tariq.92@live.com>
2022-09-09 15:41:05 +0300

    added jwt

---
+++ routes/verifyToken.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+
+function auth(req, res, next) {
+    const token = req.header("auth-token");
+    if (!token) {
+        return res.status(401).send("Unauthorized");
+    }
+
+    try {
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = verified;
+        next();
+    } catch (e) {
+        return res.status(400).send("Invalid C redentials");
+    }
+}
